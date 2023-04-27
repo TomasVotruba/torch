fd866b29fdf06afd0ba2b95c40eabcb8ecaca587
Tomas Votruba <tomas.vot@gmail.com>
2023-04-27 16:03:05 +0200

    misc

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,15 +2,15 @@
 
 declare(strict_types=1);
 
+use App\Console\ConsoleKernel;
 use Illuminate\Foundation\Application;
 use Illuminate\Contracts\Debug\ExceptionHandler;
-use Illuminate\Foundation\Console\Kernel;
 use Illuminate\Foundation\Exceptions\Handler;
 
 $application = new Application( $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__));
 
 // basic bindings
-$application->singleton(Illuminate\Contracts\Console\Kernel::class, Kernel::class);
+$application->singleton(Illuminate\Contracts\Console\Kernel::class, ConsoleKernel::class);
 $application->singleton(ExceptionHandler::class, Handler::class);
 
 return $application;
--- config/app.php
+++ config/app.php
@@ -2,5 +2,24 @@
 
 declare(strict_types=1);
 
+use Illuminate\Cache\CacheServiceProvider;
+use Illuminate\Database\DatabaseServiceProvider;
+use Illuminate\Filesystem\FilesystemServiceProvider;
+use Illuminate\Foundation\Providers\ConsoleSupportServiceProvider;
+use Illuminate\Foundation\Providers\FoundationServiceProvider;
+use Illuminate\Queue\QueueServiceProvider;
+use Illuminate\Session\SessionServiceProvider;
+use Illuminate\View\ViewServiceProvider;
+
 return \TomasVotruba\PunchCard\AppConfig::make()
+    ->providers([
+        CacheServiceProvider::class,
+        ConsoleSupportServiceProvider::class,
+        DatabaseServiceProvider::class,
+        FilesystemServiceProvider::class,
+        FoundationServiceProvider::class,
+        ViewServiceProvider::class,
+        SessionServiceProvider::class,
+        QueueServiceProvider::class,
+    ])
     ->toArray();
