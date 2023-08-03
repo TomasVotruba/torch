53bc9f4dd0f0dc6bd587d7e6f86a53976c450635
Tomas Votruba <tomas.vot@gmail.com>
2023-08-03 14:44:33 +0200

    cs

--- build/rector-downgrade-php-72.php
+++ build/rector-downgrade-php-72.php
@@ -6,8 +6,6 @@ use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\DowngradeLevelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-
-
     $rectorConfig->sets([DowngradeLevelSetList::DOWN_TO_PHP_72]);
 
     // skip tests in vendor packages
