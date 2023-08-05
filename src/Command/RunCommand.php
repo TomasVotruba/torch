066f713981c73f6f9a8be03f097b42d11638daca
Tomas Votruba <tomas.vot@gmail.com>
2023-08-05 17:00:37 +0200

    misc

--- src/Command/RunCommand.php
+++ src/Command/RunCommand.php
@@ -24,7 +24,7 @@ final class RunCommand extends Command
         private readonly TwigFileFinder $twigFileFinder,
         private readonly SymfonyStyle $symfonyStyle,
     ) {
-        $this->cacheDirectory = sys_get_temp_dir(). '/torch_twig_cache';
+        $this->cacheDirectory = sys_get_temp_dir() . '/torch_twig_cache';
 
         parent::__construct();
     }
