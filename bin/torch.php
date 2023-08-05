8eeb267689570ec1a05c8f1884e4fb4fee811611
Tomas Votruba <tomas.vot@gmail.com>
2023-08-05 15:39:10 +0200

    misc

--- bin/torch.php
+++ bin/torch.php
@@ -7,10 +7,6 @@ use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use TomasVotruba\Torch\DependencyInjection\TorchContainerFactory;
 
-if (\file_exists(__DIR__ . '/../../../../vendor/autoload.php')) {
-    // project's autoload
-    require_once __DIR__ . '/../../../../vendor/autoload.php';
-}
 if (\file_exists(__DIR__ . '/../vendor/scoper-autoload.php')) {
     // A. build downgraded package
     require_once __DIR__ . '/../vendor/scoper-autoload.php';
