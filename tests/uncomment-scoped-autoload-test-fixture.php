34c99d9997b3626e4f82e4cf1d7d7317c06da672
Tomas Votruba <tomas.vot@gmail.com>
2023-08-05 17:21:57 +0200

    add testing filega

---
+++ tests/uncomment-scoped-autoload-test-fixture.php
@@ -0,0 +1,15 @@
+<?php
+
+// Class aliases. For more information see:
+// https://github.com/humbug/php-scoper/blob/master/docs/further-reading.md#class-aliases
+if (!function_exists('humbug_phpscoper_expose_class')) {
+    function humbug_phpscoper_expose_class(string $exposed, string $prefixed): void {
+        if (!class_exists($exposed, false) && !interface_exists($exposed, false) && !trait_exists($exposed, false)) {
+            spl_autoload_call($prefixed);
+        }
+    }
+}
+
+// if (!function_exists('twig_var_dump')) { function twig_var_dump() { return \Torch202308\twig_var_dump(...func_get_args()); } }
+
+return $loader;
