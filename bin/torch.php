5f39942cc8237d72057b5119f00aa43a80af3f2c
Tomas Votruba <tomas.vot@gmail.com>
2023-08-05 15:18:57 +0200

    fix loading

--- bin/torch.php
+++ bin/torch.php
@@ -7,7 +7,17 @@ use Symfony\Component\Console\Input\ArgvInput;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use TomasVotruba\Torch\DependencyInjection\TorchContainerFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
+if (\file_exists(__DIR__ . '/../../../../vendor/autoload.php')) {
+    // project's autoload
+    require_once __DIR__ . '/../../../../vendor/autoload.php';
+}
+if (\file_exists(__DIR__ . '/../vendor/scoper-autoload.php')) {
+    // A. build downgraded package
+    require_once __DIR__ . '/../vendor/scoper-autoload.php';
+} else {
+    // B. local repository
+    require_once __DIR__ . '/../vendor/autoload.php';
+}
 
 $torchContainerFactory = new TorchContainerFactory();
 $container = $torchContainerFactory->create();
