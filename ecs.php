fd5f76fc77bf3785bf44e6859eb2da88d360faaa
Tomas Votruba <tomas.vot@gmail.com>
2024-01-29 12:47:33 +0100

    bump ecs

--- ecs.php
+++ ecs.php
@@ -2,22 +2,12 @@
 
 declare(strict_types=1);
 
-use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
-use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return function (ECSConfig $ecsConfig): void {
-    $ecsConfig->paths([
+return ECSConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
-    ]);
-
-    $ecsConfig->sets([
-        SetList::SPACES,
-        SetList::ARRAY,
-        SetList::DOCBLOCK,
-        SetList::NAMESPACES,
-        SetList::COMMENTS,
-        SetList::PSR_12,
-    ]);
-};
+    ])
+    ->withRootFiles()
+    ->withPreparedSets(psr12: true, common: true);
