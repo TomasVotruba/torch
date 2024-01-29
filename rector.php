335b690c5de78fc7862e6e3f655c694717903b47
Tomas Votruba <tomas.vot@gmail.com>
2024-01-29 15:21:38 +0100

    make use of new rector config

--- rector.php
+++ rector.php
@@ -4,21 +4,12 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
 
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withPaths([
         __DIR__ . '/src',
         __DIR__ . '/tests',
-    ]);
-
-    $rectorConfig->importNames();
-
-    $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_81,
-        SetList::TYPE_DECLARATION,
-        SetList::CODE_QUALITY,
-        SetList::CODING_STYLE,
-        SetList::NAMING,
-    ]);
-};
+    ])
+    ->withImportNames(removeUnusedImports: true)
+    ->withPreparedSets(typeDeclarations: true, codingStyle: true, naming: true, codeQuality: true)
+    ->withSets([LevelSetList::UP_TO_PHP_81]);
